e45238c0de8f6d256a6fb78b7b0d7f78e6c81ee9
Prachi <prachipatil2792002@gmail.com>
2024-02-27 22:09:49 +0530

    first commit

---
+++ constants/redirectResMessage.js
@@ -0,0 +1,9 @@
+module.exports.redirectSuccessMessages = {
+    SUCCESSFUL_INSERTION: "Data inserted Successfully"
+};
+
+module.exports.redirectErrorMessages = {
+    INTERNAL_SERVER_ERROR: "Internal Server error",
+    UNSUCCESSFUL_INSERTION_ERROR: "Data insertion failed",
+    DATA_VALIDATION_ERROR:"Data validation failed"
+};
